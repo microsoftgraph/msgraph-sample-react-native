61bcf8afed4e38a9ed56a246b5118676baf0b919
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-04-14 14:44:19 -0400

    Delete old demos

--- demos/03-add-msgraph/GraphTutorial/graph/GraphAuthProvider.js
+++
@@ -1,9 +0,0 @@
-import { AuthManager } from '../auth/AuthManager';
-
-// Used by Graph client to get access tokens
-// See https://github.com/microsoftgraph/msgraph-sdk-javascript/blob/dev/docs/CustomAuthenticationProvider.md
-export class GraphAuthProvider {
-  getAccessToken = async() => {
-    return await AuthManager.getAccessTokenAsync();
-  }
-}
